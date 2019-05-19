9fde47827ff35de5adc739272368e12353063dad
Sébastien Saunier <seb@saunier.me>
2019-05-19 13:00:15 +0200

    Simplify controller

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   onPostSuccess(event) {
     let [data, status, xhr] = event.detail;
-    this.element.innerHTML = xhr.response;
+    this.element.innerHTML = data;
   }
 }
